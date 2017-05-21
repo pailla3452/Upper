01a69eb617f0fd9b4a11df0e2a18d9f3b88e3cab
unknown <nachopaillalef@gmail.com>
2017-05-21 18:26:06 +0200

    excelente

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<!-- src/components/App.vue -->
+<template>
+  <div class="ui secondary  menu">
+    <a class="item"><i class="steam icon"></i></a>
+    <a class="active item">
+      Home
+    </a>
+    <a class="item">
+      Messages
+    </a>
+    <a class="item">
+      Friends
+    </a>
+    <button class="ui button"><i class="copy icon"></i></button>
+    <div class="right menu">
+      <div class="item">
+        <div class="ui icon input">
+          <input type="text" placeholder="Search...">
+          <i class="bug icon"></i>
+        </div>
+      </div>
+      <a class="ui item">
+        Logout
+      </a>
+    </div>
+  </div>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <style>
+    #icoc{margin-left: 100px; margin-bottom: 30px;}
+    body{background-image:url("https://www.walldevil.com/wallpapers/h12/web-background-wallpaper-copy1-templates-desktop-wallpapers-website.jpg") }
+
+  </style>
+  <div class="ui container" id="All">
+    <div class="ui vertically divided grid">
+      <div class="three column row">
+        <div class="column">
+        </div>
+        <div class="column" >
+          <i class="massive steam square icon" id="icoc"></i>
+          <form class="ui form" id="pene" method="post" >
+            <h4 class="ui dividing header">Formulario de Registro</h4>
+            <div class="field">
+              <label>Nombre</label>
+              <div class="field">
+                <div class="field">
+                  <input type="text" name="nombre" placeholder="xxxxxxx">
+                </div>
+              </div>
+            </div>
+            <div class="field">
+              <label>Correo electrónico</label>
+              <div class="field">
+                <input type="text" name="correo" placeholder="xxxxxxxxxx@gmail.com">
+              </div>
+            </div>
+            <div class="field">
+            <label>Contraseña</label>
+            </div>
+            <div class="two fields">
+              <div class="field">
+                <input type="password" name="contraseña" placeholder="Contraseña">
+              </div>
+              <div class="field">
+                <input type="password" name="contraseña2" placeholder="Verificar Contraseña">
+              </div>
+            </div>
+            <div class="ui error message">
+            </div>
+            <div class="ui button submit" id="btn-enviar">Registrarse</div>
+            <div class="ui button submit" id="btn-login">Iniciar sesión</div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+</template>
+
+<script>
+//If Error JQUERY
+$(function(){
+  $('#pene').form({
+      nombre : {
+        identifier:'nombre',
+        rules:[
+          {
+            type: 'empty',
+            prompt:'Por favor ingrese un nombre válido'
+          }
+          ]
+        },
+        correo: {
+          identifier:'correo',
+          rules:[{
+            type:'email',
+            prompt:'Ingrese un email válido'
+          }]
+        },
+        contraseña:{
+          identifier:'contraseña',
+          rules:[{
+            type:'minLength[4]',
+            prompt:'Su contraseña debe tener al menos 4 caracteres'
+          }]
+        },
+        contraseña2:{
+          identifier:'contraseña2',
+          rules:[{
+            type:'match[contraseña]',
+            prompt:'Las contraseñas no coinciden'
+          }]
+        }
+      },{
+        onSuccess:function(e){
+          e.preventDefault();
+          alert('enviando formulario');
+        }
+      });
+});
+//Base de datos VUE
+
+</script>
