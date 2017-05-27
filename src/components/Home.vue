62c699510206d91617d6ef8e135ec4dd5bc4b3ec
RO ;P <robertogc99@hotmail.com>
2017-05-27 21:14:20 +0200

    semantic and home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,94 @@
 <template>
+  <div class="ui container">
+    <div class="ui vertically divided grid">
+      <div class="three column row">
+        <div class="column">
+        </div>
+        <div class="column">
+          <div class="ui card">
+            <div class="image">
+              <img src="http://s3.amazonaws.com/cgmagazine/2015/05/cover-ps4-the-witcher-3-wild-hunt.jpg">
+            </div>
+            <div class="content">
+              <a class="header">Kristy</a>
+              <div class="meta">
+                <span class="date">Joined in 2013</span>
+              </div>
+              <div class="description">
+                Kristy is an art director living in New York.
+              </div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i class="user icon"></i>
+                22 Friends
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ui container">
+    <div class="ui vertically divided grid">
+      <div class="three column row">
+        <div class="column">
+        </div>
+        <div class="column">
+          <div class="ui card">
+            <div class="image">
+              <img src="http://s3.amazonaws.com/cgmagazine/2015/05/cover-ps4-the-witcher-3-wild-hunt.jpg">
+            </div>
+            <div class="content">
+              <a class="header">Kristy</a>
+              <div class="meta">
+                <span class="date">Joined in 2013</span>
+              </div>
+              <div class="description">
+                Kristy is an art director living in New York.
+              </div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i class="user icon"></i>
+                22 Friends
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ui container">
+    <div class="ui vertically divided grid">
+      <div class="three column row">
+        <div class="column">
+        </div>
+        <div class="column">
+          <div class="ui card">
+            <div class="image">
+              <img src="http://s3.amazonaws.com/cgmagazine/2015/05/cover-ps4-the-witcher-3-wild-hunt.jpg">
+            </div>
+            <div class="content">
+              <a class="header">Kristy</a>
+              <div class="meta">
+                <span class="date">Joined in 2013</span>
+              </div>
+              <div class="description">
+                Kristy is an art director living in New York.
+              </div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i class="user icon"></i>
+                22 Friends
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
