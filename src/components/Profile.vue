3e3f3c9ef1a655181099a43bc5d6987494d187e9
pailla3452 <danigilchu@gmail.com>
2017-05-28 18:15:02 +0200

    profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="ui container">
+    <div class="ui vertically divided grid">
+      <div class="three column row">
+      </div>
+    </div>
+  </div>
+
+</h2>
+</template>
+<script>
+</script>
